bdf2395e1ffe84493ac388fe72fa322da434d7e8
Prizma <prizmashrestha19@gmail.com>
2024-05-29 13:01:47 +0545

    que4

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL="/"
+LOGIN_URL='/login/'
+LOGOUT_REDIRECT_URL = '/login/'
